a1e089e51492451a4b433b235b53a538e796a4ed
Joseph Crachiolo <crachioloj@gmail.com>
2020-05-05 17:32:41 -0400

    Styling

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,21 +1,7 @@
-// import "./utils.js";
-// import subtract, { square, add } from "./utils.js";
+import React from "react";
+import ReactDOM from "react-dom";
+import IndecisionApp from "./components/IndecisionApp";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
 
-// console.log("app.js is running");
-// console.log(square(4))
-// console.log(add(2,3));
-// console.log(subtract(100, 81)); //subtract, name doesn't matter for default export
-
-import isSenior, { isAdult, canDrink } from "./person.js";
-
-console.log(isAdult(17));
-console.log(isAdult(18));
-console.log(isAdult(21));
-
-console.log(canDrink(18));
-console.log(canDrink(21));
-console.log(canDrink(33));
-
-console.log(isSenior(64));
-console.log(isSenior(65));
-console.log(isSenior(99));
+ReactDOM.render(<IndecisionApp/>, document.getElementById("root"));
