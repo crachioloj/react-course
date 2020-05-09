066f96e2ce97c47a3effc4a56eb6add8484c3d0e
Joseph Crachiolo <crachioloj@gmail.com>
2020-05-09 15:49:56 -0400

    Cleanup

--- portfolio-site/src/components/PortFolioItem.js
+++ portfolio-site/src/components/PortFolioItem.js
@@ -3,7 +3,7 @@ import React from "react";
 const PortFolioItem = (props) => (
   <div>
     <h1>A Thing I've done</h1>
-    This is the page for the item with id of {props.match.params.id}
+    <p>This is the page for the item with id of {props.match.params.id}</p>
   </div>
 );
 
--- portfolio-site/src/components/PortfolioPage.js
+++ portfolio-site/src/components/PortfolioPage.js
@@ -4,11 +4,9 @@ import { Link } from "react-router-dom";
 const PortfolioPage = () => (
   <div>
     <h1>My Work</h1>
-    Check out the following things I've done:
-    <div>
-      <Link to="/portfolio/1">Item 1</Link>
-      <Link to="/portfolio/2">Item 2</Link>
-    </div>
+    <p>Check out the following things I've done:</p>
+    <Link to="/portfolio/1">Item 1</Link>
+    <Link to="/portfolio/2">Item 2</Link>
   </div>
 );
 
